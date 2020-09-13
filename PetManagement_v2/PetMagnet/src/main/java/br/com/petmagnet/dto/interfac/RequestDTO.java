c1a145534ed83e37f13ce4ff3ce6c216c96f25b7
SidneiVGomes <sidneivgomes@gmail.com>
2020-09-13 19:22:40 -0300

    Ajuste do Repositório

--- PetManagement_v2/PetMagnet/src/main/java/br/com/petmagnet/dto/interfac/RequestDTO.java
+++
@@ -1,5 +0,0 @@
-package br.com.petmagnet.dto.interfac;
-
-public interface RequestDTO {
-	public Object toEntity();
-}
--- PetManagement_v2/PetMagnet/src/main/java/br/com/petmagnet/dto/interfac/ResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.petmagnet.dto.interfac;
-
-import java.util.List;
-
-public interface ResponseDTO {
-	public List<Object> toList();
-}
